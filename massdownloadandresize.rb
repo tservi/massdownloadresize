8a76783d0b473d30975c28769d79bfae542aac6e
Jean Tinguely Awais <jean@local@t-servi.com>
2014-08-11 13:07:41 +0200

    retaille des images et effacement des originaux

--- massdownloadandresize.rb
+++ massdownloadandresize.rb
@@ -6,21 +6,29 @@ max = 125
 url1 = "https://content.wuala.com/contents/"
 url2 = "Heinz_Wyler/Fotosammlung%20Heinz/Auftragsarbeiten/Bauprojekt%20neues%20Geburtshaus%20im%20Petit%20Moncor/Neues%20Geburtshaus/"
 url3 = "%20PetitPrince.jpg/?dl=1&key=sU7ylQDOEwyD"
-name = "PetitPrince.jpg"
+complement = "PetitPrince.jpg"
 
 require 'open-uri'
+require 'rubygems'
+require 'ImageResize' # gem install ImageResize
+
 
 # préparation des urls
 urls = Hash.new
 (min..max).to_a.each {
   |e|
-  urls[ "#{format("%03d",e)}_" + name ] = url1 + url2 + "#{format("%03d",e)}" + url3
+  urls[ "#{format("%03d",e)}_" + complement ] = url1 + url2 + "#{format("%03d",e)}" + url3
   }
 
-#rapatriement des images
+#rapatriement et taille des images
 urls.each {|name, url |
   #puts name, url
-  open('images/' + name , 'wb') do |file|
-    file << open(url).read
-  end
+  image = open('images/' + name + '.original.jpg' , 'wb')
+  image << open(url).read
+  Image.resize( 'images/' + name + '.original.jpg'  , 'images/' + name , 640, 640)
 }
+
+# nettoyage du répertoire
+FileUtils.rm Dir.glob('images/*original*')
+
+# au fait, inutile d'essayer sur MS windows open-uri fonctionne très mal ... ;-)
